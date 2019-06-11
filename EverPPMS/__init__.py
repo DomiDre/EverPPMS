41146f97aa9a63469927e35661f3d5034cc051a7
DomiDre <homiedomi@gmail.com>
2019-06-11 10:54:58 +0200

    Add closest_idx to lib

--- EverPPMS/__init__.py
+++ EverPPMS/__init__.py
@@ -1,3 +1,3 @@
-from ._lib import generate_FORC_sequence, generate_IRM_DCD_sequence, get_cmap
+from ._lib import generate_FORC_sequence, generate_IRM_DCD_sequence, get_cmap, closest_idx
 from ._forc import FORC
 from ._irmdcd import IRMDCD
--- EverPPMS/_lib.py
+++ EverPPMS/_lib.py
@@ -1,4 +1,6 @@
 import matplotlib.colors as mcolors
+import numpy as np
+
 
 def generate_FORC_sequence(
   sequence_filename,
@@ -136,3 +138,18 @@ def get_cmap():
   cmap =  mcolors.LinearSegmentedColormap('CustomMap', cdict)
   cmap.set_bad(color='black')
   return cmap
+
+def closest_idx(array, value):
+  idx_sorted = np.argsort(array)
+  sorted_array = np.array(array[idx_sorted])
+  idx = np.searchsorted(sorted_array, value, side="left")
+  if idx >= len(array):
+    idx_nearest = idx_sorted[len(array)-1]
+  elif idx == 0:
+    idx_nearest = idx_sorted[0]
+  else:
+    if abs(value - sorted_array[idx-1]) < abs(value - sorted_array[idx]):
+      idx_nearest = idx_sorted[idx-1]
+    else:
+      idx_nearest = idx_sorted[idx]
+  return idx_nearest
